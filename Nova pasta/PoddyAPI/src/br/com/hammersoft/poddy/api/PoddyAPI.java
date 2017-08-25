ce01ff193bad7c3b85dd73af2de9741909c7b7ef
jmtst <jmtst@192.168.0.4>
2017-08-25 08:07:49 -0300

    fdhgfdfg

--- Nova pasta/PoddyAPI/src/br/com/hammersoft/poddy/api/PoddyAPI.java
+++ Nova pasta/PoddyAPI/src/br/com/hammersoft/poddy/api/PoddyAPI.java
@@ -2,28 +2,48 @@ package br.com.hammersoft.poddy.api;
 
 import java.net.URL;
 
+import com.google.gson.Gson;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
 import br.com.hammersoft.net.http.HttpURLConnector;
+import br.com.hammersoft.poddy.api.pojo.ResultList;
 
 public class PoddyAPI {
 	
-	//https://www.mixcloud.com/developers/
-	//https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+	private static final String API_ADDR = "https://itunes.apple.com/";
+	private static final String API_SEARCH = "search";
+	private static final String API_PARAM_INIT = "?";
+	private static final String API_PARAM_SEP = "&";
+	private static final String API_PARAM_TERM = "term=";
+	private static final String API_PARAM_MEDIA = "media=";
 	
-
 	public PoddyAPI() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private static ResultList searchMedia(String term, String mediaType) throws Exception{
+		String url = 
+				API_ADDR + 
+				API_SEARCH + 
+				API_PARAM_INIT +
+				API_PARAM_TERM +
+				term +
+				(mediaType != null ? 
+						API_PARAM_SEP + 
+						API_PARAM_MEDIA + mediaType : "");
+		
+		
+		String response = HttpURLConnector.sendGet(url);
+		Gson gson = new Gson();
+		return gson.fromJson(response, ResultList.class);
 	}
 
 	public static void main(String[] args) {
 		try {
-			System.out.println(HttpURLConnector.sendGet("http://api.digitalpodcast.com/v2r/search/?appid=podcastsearchdemo&keywords=rock%20and%20roll&format=opml&sort=rating&searchsource=title&contentfilter=noadult&start=1&results=3"));
-			
-			String url = "https://stackoverflow.com/feeds/tag?tagnames=rome";
-			SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL("http://www.americanheartbreak.com/rnrgeekwp/feed/")));
+			String url = PoddyAPI.searchMedia("Jovem+Nerd", null).getResults().get(0).getFeedUrl();
+			SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
 			System.out.println(feed.getTitle());
 		} catch (Exception e) {
 			e.printStackTrace();
